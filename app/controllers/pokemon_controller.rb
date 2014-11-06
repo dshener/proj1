4ae1c3067f50dc61c0fdd52db1e657ddb23deba8
Declan Shener <dshener1@berkeley.edu>
2014-11-06 01:26:20 -0800

    I am a Pokemon master.

---
+++ app/controllers/pokemon_controller.rb
@@ -0,0 +1,40 @@
+class PokemonController < ApplicationController
+	def new
+	end
+
+	def create
+    	@pokemon = Pokemon.new(pokemon_params)
+    	@pokemon.health = 100
+    	@pokemon.level = 1
+    	@pokemon.trainer_id = current_trainer.id
+    	if @pokemon.valid?
+    		@pokemon.save
+      		redirect_to trainer_path(id: current_trainer.id)
+    	else
+      		flash[:error] = @pokemon.errors.full_messages.to_sentence
+      		redirect_to new_pokemon_path
+    	end
+  	end
+
+	def capture
+		@pokemon = Pokemon.find(params[:id])
+		if @pokemon.update_attribute(:trainer_id, current_trainer.id)
+			redirect_to root_path
+		end
+	end
+
+	def damage
+    	@pokemon = Pokemon.find(params[:id])
+		@pokemon.update_attribute(:health, @pokemon.health-10)
+		if @pokemon.health<=0
+			@pokemon.destroy
+		end
+		redirect_to trainer_path(current_trainer.id)
+	end
+
+	private
+
+  	def pokemon_params
+    	params.require(:pokemon).permit(:name, :level, :health, :trainer_id)
+  	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   root to: 'home#index'
   devise_for :trainers
   resources :trainers
+  resources :pokemons
+
+  patch 'pokemon/:id', to:'pokemon#capture', as:'capture'
+  delete 'pokemon/:id',to:'pokemon#damage', as:'damage'
+  get 'new', to: 'pokemon#new', as: 'new'
 end
